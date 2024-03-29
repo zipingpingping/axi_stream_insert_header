1aac31c2e51a6be521ecd13fd8dd76c1e9f30875
zipingpingping <141007215+zipingpingping@users.noreply.github.com>
2024-03-29 22:11:04 +0800

    axi_stream_insert_header.v

--- axi_stream_insert_header.v
+++ axi_stream_insert_header.v
@@ -3,37 +3,37 @@ module axi_stream_insert_header #(
 		parameter DATA_BYTE_WD = DATA_WD / 8,
 		parameter BYTE_CNT_WD = $clog2(DATA_BYTE_WD)
 ) (
-		input 								clk,
-		input 								rst_n,
+		input 										clk,
+		input 										rst_n,
 		
 		//	AXI Stream input original data
-		input 								valid_in,
-		input [DATA_WD-1 : 0] 			data_in,
-		input [DATA_BYTE_WD-1 : 0] 	keep_in,
-		input 								last_in,
-		output 								ready_in,
+		input 										valid_in,
+		input [DATA_WD-1 : 0] 								data_in,
+		input [DATA_BYTE_WD-1 : 0] 							keep_in,
+		input 										last_in,
+		output 										ready_in,
 		
 		// AXI Stream output with header inserted
-		output 								valid_out,
-		output [DATA_WD-1 : 0] 			data_out,
-		output [DATA_BYTE_WD-1 : 0] 	keep_out,
-		output 								last_out,
-		input 								ready_out,
+		output 										valid_out,
+		output [DATA_WD-1 : 0] 								data_out,
+		output [DATA_BYTE_WD-1 : 0] 							keep_out,
+		output 										last_out,
+		input 										ready_out,
 
 		// The header to be inserted to AXI Stream input
-		input 								valid_insert,
-		input [DATA_WD-1 : 0] 			data_insert,
-		input [DATA_BYTE_WD-1 : 0] 	keep_insert,
-		input [BYTE_CNT_WD-1 : 0] 		byte_insert_cnt,
-		output 								ready_insert
+		input 										valid_insert,
+		input [DATA_WD-1 : 0] 								data_insert,
+		input [DATA_BYTE_WD-1 : 0] 							keep_insert,
+		input [BYTE_CNT_WD-1 : 0] 							byte_insert_cnt,
+		output 										ready_insert
 		);
 
 // Your code here	
 		
 /*
-***********************************************************************************
+**********************************************************************************************************************************************************************
 								  data	channel	logic	begin
-***********************************************************************************
+**********************************************************************************************************************************************************************
 */
 
 //ready_in	logic				
@@ -52,16 +52,16 @@ else		begin
 end
 
 //find the rise edge	of	tlast
-wire											data_tlast_rise_edge;
-assign										data_tlast_rise_edge = last_in1 & (~last_in2);
+wire												data_tlast_rise_edge;
+assign												data_tlast_rise_edge = last_in1 & (~last_in2);
 
-wire											ready_in_r;
-assign										ready_in_r = ~data_tlast_rise_edge;
-assign										ready_in   = ready_in_r;
+wire												ready_in_r;
+assign												ready_in_r = ~data_tlast_rise_edge;
+assign												ready_in   = ready_in_r;
 
 //cache the keep_in
-wire		[DATA_BYTE_WD-1 : 0] 		keep_reg;
-assign										keep_reg = (!rst_n)?'d0 : (last_in1)?keep_in:keep_reg;
+wire			[DATA_BYTE_WD-1 : 0] 			keep_reg;
+assign												keep_reg = (!rst_n)?'d0 : (last_in1)?keep_in:keep_reg;
 
 //data_in	logic
 
@@ -86,60 +86,60 @@ else
 end	
 
 /*
-***********************************************************************************
+**********************************************************************************************************************************************************************
 								  data	channel	logic	end
-***********************************************************************************
+**********************************************************************************************************************************************************************
 */
 
 
 /*
-***********************************************************************************
+**********************************************************************************************************************************************************************
 								  header	channel	logic	begin
-***********************************************************************************
+**********************************************************************************************************************************************************************
 */
 
 //data_insert	logic				
 
 reg		[DATA_WD-1 : 0] 				data_insert_r;
-reg		[DATA_BYTE_WD-1 : 0] 		keep_insert_r;
-reg											insert_busy;
+reg		[DATA_BYTE_WD-1 : 0] 				keep_insert_r;
+reg								insert_busy;
 
 always@(posedge	clk	or	negedge	rst_n)begin
 if(!rst_n)begin
 												data_insert_r <= 'd0;
-												keep_insert_r    <= 'd0;
-												insert_busy	  <= 'd0;
+												keep_insert_r <= 'd0;
+												insert_busy   <= 'd0;
 
 end
 else	if(valid_insert&&ready_insert)begin
 							case(keep_insert)
 									4'b1111 :begin
 												data_insert_r <= data_insert;
-												keep_insert_r	  <= keep_insert;
+												keep_insert_r <= keep_insert;
 												insert_busy   <= 'd1;
 												end
 									
 									4'b0111:	begin
 												data_insert_r <= {8'd0,data_insert[23:0]};
-												keep_insert_r	  <= keep_insert;
+												keep_insert_r <= keep_insert;
 												insert_busy   <= 'd1;
 												end
 										
 									4'b0011:	begin
 												data_insert_r <= {16'd0,data_insert[15:0]};
-												keep_insert_r	  <= keep_insert;
+												keep_insert_r <= keep_insert;
 												insert_busy   <= 'd1;
 												end
 									
 									4'b0001:	begin
 												data_insert_r <= {24'd0,data_insert[7:0]};
-												keep_insert_r	  <= keep_insert;
+												keep_insert_r <= keep_insert;
 												insert_busy   <= 'd1;
 												end
 									
 									default:	begin
 												data_insert_r <= data_insert_r;
-												keep_insert_r	  <= keep_insert;
+												keep_insert_r <= keep_insert;
 												insert_busy   <= 'd1;
 												end
 							endcase
@@ -153,7 +153,7 @@ end
 								
 //ready_insert	logic			
 	
-reg											ready_insert_r;								
+reg								ready_insert_r;								
 always@(posedge	clk	or	negedge	rst_n)
 if(!rst_n)
 												ready_insert_r <= 'd0;
@@ -162,10 +162,10 @@ else	if(valid_insert&&ready_insert)
 else
 												ready_insert_r <= 'd1;
 												
-assign										ready_insert   =   ready_insert_r;			
+assign												ready_insert   =   ready_insert_r;			
 
 //cache	the keep_insert
-reg			[DATA_BYTE_WD-1 : 0]		keep_insert_reg;
+reg			[DATA_BYTE_WD-1 : 0]			keep_insert_reg;
 always@(posedge	clk	or	negedge	rst_n)
 if(!rst_n)		
 												keep_insert_reg <= 'd0;
@@ -176,16 +176,16 @@ else
 		
 			
 /*
-***********************************************************************************
+**********************************************************************************************************************************************************************
 								  header	channel	logic	end
-***********************************************************************************
+**********************************************************************************************************************************************************************
 */
 							
 
 /*
-***********************************************************************************
+**********************************************************************************************************************************************************************
 								  header	insert in data	logic	begin
-***********************************************************************************
+**********************************************************************************************************************************************************************
 */
 
 //data_in+header	logic
@@ -198,7 +198,7 @@ if(!rst_n)begin
 else	if(insert_busy)begin												//insert	header	stage
 								case(keep_insert)
 									4'b1111 :begin
-												data_out_r 	<= data_insert_r;
+												data_out_r <= data_insert_r;
 												end
 												
 									4'b0111:	begin
@@ -221,11 +221,11 @@ else	if(insert_busy)begin												//insert	header	stage
 else	begin																	//transfer	datain
 								case(keep_insert_r)
 									4'b1111 :begin
-												data_out_r 	<= data_in_r2;
+												data_out_r <= data_in_r2;
 												end
 												
 									4'b0111:	begin
-												data_out_r  <= {data_in_r2[23:0],data_in_r1[31:24]};
+												data_out_r <= {data_in_r2[23:0],data_in_r1[31:24]};
 												end
 										
 									4'b0011:	begin
@@ -244,20 +244,20 @@ else	begin																	//transfer	datain
 							end
 end					
 
-assign										data_out = ready_out?data_out_r:data_in_r2;	
+assign												data_out = ready_out?data_out_r:data_in_r2;	
 						
 
 //last_out	logic											
-reg											lag_1clk_signal;
+reg									lag_1clk_signal;
 always@(posedge	clk	or	negedge	rst_n)
 if(!rst_n)
 												lag_1clk_signal <= 'd0;
 else
 												lag_1clk_signal <= data_tlast_rise_edge;
 												
-wire											last_out_r;
-assign										last_out_r = (~data_tlast_rise_edge)&lag_1clk_signal;											
-assign										last_out   = last_out_r;	
+wire												last_out_r;
+assign												last_out_r = (~data_tlast_rise_edge)&lag_1clk_signal;											
+assign												last_out   = last_out_r;	
 
 											
 //keep_out	logic
@@ -268,10 +268,10 @@ if(!rst_n)
 else	if(valid_out)begin
 			if(last_out_r)begin
 								case(keep_insert_reg)
-									4'b1111:	keep_out_r <= keep_reg   ;
-									4'b0111:	keep_out_r <= keep_reg<<1;
-									4'b0011:	keep_out_r <= keep_reg<<2;
-									4'b0001:	keep_out_r <= keep_reg<<3;
+									4'b1111:		keep_out_r <= keep_reg   ;
+									4'b0111:		keep_out_r <= keep_reg<<1;
+									4'b0011:		keep_out_r <= keep_reg<<2;
+									4'b0001:		keep_out_r <= keep_reg<<3;
 									default:							 		 ;
 								endcase
 							end
@@ -281,11 +281,11 @@ else	if(valid_out)begin
 else
 												keep_out_r <= 'd0;
 																					
-assign										keep_out = keep_out_r;
+assign												keep_out = keep_out_r;
 
 
 //valid_out	logic
-reg											valid_out_r;
+reg									valid_out_r;
 always@(posedge	clk	or	negedge	rst_n)
 if(!rst_n)					
 												valid_out_r<='d0;
@@ -296,12 +296,12 @@ else	if(valid_out_r=='d1&&data_tlast_rise_edge)
 else
 												valid_out_r<=valid_out_r;
 
-assign										valid_out = valid_out_r;
+assign												valid_out = valid_out_r;
 
 /*
-***********************************************************************************
+**********************************************************************************************************************************************************************
 								  header	insert in data	logic	end
-***********************************************************************************
+**********************************************************************************************************************************************************************
 */
 
 
